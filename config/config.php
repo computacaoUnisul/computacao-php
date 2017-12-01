d71e8786c37b1613684ecf9903aaedec7ba2005e
Tiago Boeing <tiagoboeing@bol.com.br>
2017-12-01 20:27:44 -0200

    Inicializa

---
+++ config/config.php
@@ -0,0 +1,16 @@
+<?php
+
+$nomeSite = "Biblioteca da Computação - Unisul - Tubarão/SC";
+$arquivoLogomarca = "";
+
+
+
+// o site será liberado ao público, ou seja, não necessita de login e senha
+$bloqueiaSite = true;
+
+
+// seta variáveis no escopo
+define("nomeSite", $nomeSite);
+define("arquivoLogo", $arquivoLogo);
+
+?>
---
+++ login.php
@@ -0,0 +1,37 @@
+<?php require("config/config.php"); ?>
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <title><?php echo nomeSite; ?></title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+
+    <!-- Font-awesome -->
+    <script src="https://use.fontawesome.com/dd1e1cb31a.js"></script>
+
+  </head>
+
+  <body>
+
+    <h2><i class="fa fa-lock"></i> Efetue login</h2>
+    <form>
+      <div class="form-group">
+        <label class="col-form-label" for="formGroupExampleInput">Example label</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+      </div>
+      <div class="form-group">
+        <label class="col-form-label" for="formGroupExampleInput2">Another label</label>
+        <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+      </div>
+    </form>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+  </body>
+</html>
