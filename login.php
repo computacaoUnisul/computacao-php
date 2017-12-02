811566d9d550709cec07fea91ac86c3ac317ce0c
Tiago Boeing <tiagoboeing@bol.com.br>
2017-12-02 00:29:37 -0200

    Formulário para telas full hd

--- login.php
+++ login.php
@@ -22,7 +22,7 @@
   <div class="container-fluid jumbotron">
     <div class="row justify-content-md-center">
 
-      <div class="col-md-3">
+      <div class="col-md-3 col-lg-2">
 
         <h2><i class="fa fa-lock"></i> Efetue login</h2>
         <p>Para acessar os materiais é necessário efetuar login.</p>
