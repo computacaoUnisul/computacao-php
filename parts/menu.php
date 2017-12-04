68794acf26f872edafa24b9177c2e8ed481b5cbb
Tiago Boeing <tiagoboeing@bol.com.br>
2017-12-04 02:41:59 -0200

    Menu com lista de professores ativos

--- parts/menu.php
+++ parts/menu.php
@@ -50,13 +50,38 @@
                 // estrutura de repetição
                 while($row = $select->fetch(PDO::FETCH_OBJ)): ?>
 
-                <li><a href="<?php echo urlSite; ?>materia/<?php echo limpaUrl($row->mat_nome); ?>"><?php echo $row->mat_nome; ?></a></li>
+                <li><a href="<?php echo urlSite; ?>materia/<?php echo $row->mat_alias; ?>"><?php echo $row->mat_nome; ?></a></li>
 
               <?php endwhile; endif;?>
 
             </ul>
 
           </li>
+
+          <li>
+            <a href="#pageSubmenuProfessores" data-toggle="collapse" aria-expanded="false">Professores</a>
+
+            <ul class="collapse list-unstyled" id="pageSubmenuProfessores">
+
+              <?php
+
+              $selectProfessores = $conn->prepare("SELECT * FROM professores WHERE prof_status <> ? AND prof_status <> ? ORDER BY prof_nome ASC");
+              $selectProfessores->execute(array("Desativado", "Pendente"));
+
+              // verifica se existem registros
+              if($selectProfessores->rowCount() > 0):
+
+                // estrutura de repetição
+                while($rowProfessores = $selectProfessores->fetch(PDO::FETCH_OBJ)): ?>
+
+                <li><a href="<?php echo urlSite; ?>professor/<?php echo $rowProfessores->prof_alias; ?>"><?php echo $rowProfessores->prof_nome." ".$rowProfessores->prof_sobrenome; ?></a></li>
+
+              <?php endwhile; endif;?>
+
+            </ul>
+
+          </li>
+
           <li>
             <a href="<?php echo urlSite; ?>qisso/">Qisso?</a>
           </li>
