892b629c52e5ca87420bcbc5cf0d2361ebccca83
Tiago Boeing <tiagoboeing@bol.com.br>
2017-12-04 03:09:54 -0200

    Página para doações e botão paypal

--- pages/qisso.php
+++ pages/qisso.php
@@ -5,3 +5,25 @@ Trabalhos, provas, todos eles separados por semestres, matérias, professores. N
 </p>
 
 <p>Esta aplicação tenta resolver estes problemas.</p>
+
+<div class="line"></div>
+
+<h5>Donate?</h5>
+<p>Tá ligado que isso dá uma trabalheira né? Se quiser ajudar com a doação de alguma quantia, manda brasa!</p>
+
+<div class="row">
+
+  <div class="col-sm-12 col-md-3 col-lg-3 col-xll-3 mb-3">
+    <div class="img-fluid" style="max-width: 450px; padding-bottom:70%;position:relative;"><iframe src="https://giphy.com/embed/3o6Zt3OhbsQ5VLPmBW" width="100%" height="100%" style="max-height: 300px; position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div>
+  </div>
+
+  <div class="col-sm-12 col-md-3 col-lg-3 col-xll-3">
+    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+    <input type="hidden" name="cmd" value="_s-xclick">
+    <input type="hidden" name="hosted_button_id" value="KRCJ3KE3X9H46">
+    <input type="image" src="https://www.paypalobjects.com/pt_BR/BR/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - A maneira fácil e segura de enviar pagamentos online!">
+    <img alt="" border="0" src="https://www.paypalobjects.com/pt_BR/i/scr/pixel.gif" width="1" height="1">
+    </form>
+  </div>
+
+</div>
